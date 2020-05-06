f0c8650d67e95875d49ed7da8359fdc7bc36d2d3
Seigenkousya <seigenkousya@outlook.jp>
2020-05-07 00:28:09 +0900

    modify clear terminal

--- src/display.cpp
+++ src/display.cpp
@@ -129,8 +129,8 @@ void display_array(uint8_t *memory,char *output){
 	x=1;
 	y=2;
 
-	std::system("clear");
-	//std::wcout << L"\033[2J" << std::flush;
+	//std::system("clear");
+	std::wcout << L"\033[2J" << std::flush;
 	//fflush(stdout);
 
 	if(address==(index_start+box_num-1)){
